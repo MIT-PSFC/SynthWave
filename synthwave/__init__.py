daaca6aa11fc311c47be04034a103a03f8c8edc3
ZanderKeith <zkeith@mit.edu>
2025-11-08 17:35:15 -0500

    Switched to naive tracing by default

--- synthwave/__init__.py
+++ synthwave/__init__.py
@@ -1,3 +1,6 @@
+import os
 from importlib.resources import files
 
 PACKAGE_ROOT = files("synthwave")
+VESSEL_CACHE_DIR = os.path.join(PACKAGE_ROOT, "vessel_caches")
+os.makedirs(VESSEL_CACHE_DIR, exist_ok=True)
