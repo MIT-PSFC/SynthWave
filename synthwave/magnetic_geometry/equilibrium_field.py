c58f1d9ef32e7649f6c7cb4d787b88355c18e276
Rian Chandra <rianc@uw.edu>
2026-01-26 16:18:42 -0500

    Synch improvements for helicity, q(r) discritization, and d_phi advance step

--- synthwave/magnetic_geometry/equilibrium_field.py
+++ synthwave/magnetic_geometry/equilibrium_field.py
@@ -42,20 +42,29 @@ def biot_savart_cylindrical(
 
 
 class EquilibriumField:
-    def __init__(self, eqdsk):
+    def __init__(self, eqdsk,lam=1e-7):
         self.eqdsk = eqdsk
         self.psi = RectBivariateSpline(
-            eqdsk.r_grid[:, 0], eqdsk.z_grid[0, :], eqdsk.psi, kx=3, ky=3, s=0
+            eqdsk.r_grid[:, 0], eqdsk.z_grid[0, :], eqdsk.psi,
+            kx=3, ky=3, s=0
         )
 
         # Linear grid of psi for 1D profiles
         # https://freeqdsk.readthedocs.io/en/stable/geqdsk.html
         self.psi_grid = np.linspace(eqdsk.simagx, eqdsk.sibdry, eqdsk.nx)
-
+        
         # q(psi)
-        self.qpsi = make_interp_spline(self.psi_grid, eqdsk.qpsi, k=3, axis=0)
+        # RNC EDIT: Switching to smoothing spline to avoid strange "discretization" jumps
+        # Note: lam value (lower = less smoothing) should be reasonably consistent across q 
+        # profiles, but this is not certain. Lam=1e-7 works for q(psi) and F(psi) so far.
+        self.qpsi = make_smoothing_spline(
+            self.psi_grid, eqdsk.qpsi, lam=lam, axis=0
+        )
+
         # F(psi)
-        self.F = make_interp_spline(self.psi_grid, eqdsk.fpol, k=3, axis=0)
+        self.F = make_smoothing_spline(
+            self.psi_grid, eqdsk.fpol, lam=lam, axis=0
+        )
 
     def get_field_at_point(self, R, Z) -> np.ndarray:
         # Bp = Br + Bz = (d(psi)/dZ - d(psi)/dR) / R
@@ -68,7 +77,7 @@ class EquilibriumField:
 
         return np.array([Br, Bt, Bz])
 
-    def get_psi_of_q(self, q, tol=1e-3):
+    def get_psi_of_q(self, q):
         """Get psi corresponding to a given q"""
         qpsi_grid = self.qpsi(self.psi_grid)
         psi_guess = self.psi_grid[np.argmin(np.abs(qpsi_grid - q))]
@@ -76,13 +85,12 @@ class EquilibriumField:
             func=lambda psi: self.qpsi(psi) - q,
             x0=psi_guess,
             fprime=lambda psi: self.qpsi.derivative(1)(psi),
-            maxiter=400,
-            tol=tol,
+            maxiter = 400,tol=1e-3
         )
 
-        # RNC EDIT:
-        # CHECK: For q~<=1, depening on the resolution of the gEQDSK file,
-        # the interpolation function can request a psi value at or less
+        # RNC EDIT: 
+        # CHECK: For q~<=1, depening on the resolution of the gEQDSK file, 
+        # the interpolation function can request a psi value at or less 
         # than the minimum in the psi_grid vector
         # Check to see if the value we want plausibly exists in the final set
         # of q values from the eqdsk
@@ -90,19 +98,11 @@ class EquilibriumField:
         # q values are "grouped" in an odd, stepwise fashion
         if psi <= self.psi_grid[0]:
             # check if there's a range of possible q-values (e.g.) if this is plausably a resolution issue
-            if (
-                np.argwhere(qpsi_grid > (qpsi_grid[0] + tol)).squeeze()[0] > 1
-            ):  # multiple identical q values in a row
-                lin_interp_q = np.polyfit(self.psi_grid[:30], qpsi_grid[:30], 1)
-                psi = self.psi_grid[
-                    np.argmin(np.abs(np.polyval(lin_interp_q, self.psi_grid[:30]) - q))
-                ]
-
-            if psi > self.psi_grid[0]:
-                return psi
-            else:
-                raise ValueError(
-                    "Error: requested q=%1.3f is outside the gEQDSK range (q_min = %1.3f)"
-                    % (q, qpsi_grid[0])
-                )
+            tol = 1e-3
+            if np.argwhere(qpsi_grid>(qpsi_grid[0]+1e-3)).squeeze()[0] > 1: # multiple identical q values in a row
+                lin_interp_q = np.polyfit(self.psi_grid[:30],qpsi_grid[:30], 1)
+                psi = self.psi_grid[ np.argmin(np.abs(np.polyval(lin_interp_q,self.psi_grid[:30]) - q)) ]
+            
+            if psi > self.psi_grid[0]: return psi
+            else: raise SyntaxError('Error: requested q=%1.3f is outside the gEQDSK range (q_min = %1.3f)'%(q,qpsi_grid[0]))
         return psi
