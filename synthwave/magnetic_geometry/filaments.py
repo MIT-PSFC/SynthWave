Corrected minor issue in the sign of d-Phi: it was not tracking helicity sign, and thus wouldn't change direction under the parallel or antiparallel transformation

synthwave/magnetic_geometry/filaments.py
@@ -539,7 +539,7 @@ class EquilibriumFilamentTracer(FilamentTracer):
             dR = np.roll(R, -1) - R
             dZ = np.roll(Z, -1) - Z
             dl = np.sqrt(dR**2 + dZ**2)
-            d_phi = _d_phi_dl(dl, R, np.sqrt(B[0] ** 2 + B[2] ** 2), B[1])
+            d_phi = _d_phi_dl(dl, R, np.sqrt(B[0] ** 2 + B[2] ** 2), B[1]) * self.helicity_sign
 
             if trace_type == EquilibriumFilamentTracer.TraceType.SINGLE:
                 phi = np.cumsum(d_phi) - d_phi[0]
@@ -551,7 +551,8 @@ class EquilibriumFilamentTracer(FilamentTracer):
             # Numerical correction to ensure final point is at the proper angle.
             # sign_Bt carries the direction of the toroidal field; helicity_sign
             # carries whether we trace parallel (+1) or antiparallel (-1) to the field.
-            known_phi_end = self.helicity_sign * sign_Bt * 2 * np.pi * m_local / n_local
+            
+            known_phi_end =  self.helicity_sign * sign_Bt * 2 * np.pi * m_local / n_local
 
             # If actual phi significantly deviates from known phis, log a critical warning
             if not np.isclose(phi[-1], known_phi_end, atol=0.5):