91859f08b2679a2992fabba79a8b55b0312f1408
ZanderKeith <zkeith@mit.edu>
2026-09-14 18:35:13 -0400

    Removed skipif since we should always know where the DIII-D geqdsk is

--- synthwave/tests/test_equilibrium_field.py
+++ synthwave/tests/test_equilibrium_field.py
@@ -262,21 +262,6 @@ class TestCocos:
                 f"Expected COCOS {expected_cocos} for C-Mod, got {cocos}"
             )
 
-        @pytest.mark.skipif(
-            condition=not os.path.exists(
-                os.path.join(
-                    PACKAGE_ROOT,
-                    "..",
-                    "submodules",
-                    "OpenFUSIONToolkit",
-                    "examples",
-                    "TokaMaker",
-                    "DIIID",
-                    "g192185.02440",
-                )
-            ),
-            reason="Test requires DIII-D data which is not open source",
-        )
         def test_detect_cocos_d3d(self, eqdsk_d3d):
             """DIII-D uses EFIT convention (sigma_RphiZ=+1, e_Bp=0, psi increasing).
             COCOS depends on sign(Ip) and sign(B0)
@@ -360,24 +345,7 @@ class TestCocos:
             "eqdsk",
             [
                 "eqdsk_cmod",
-                pytest.param(
-                    "eqdsk_d3d",
-                    marks=pytest.mark.skipif(
-                        condition=not os.path.exists(
-                            os.path.join(
-                                PACKAGE_ROOT,
-                                "..",
-                                "submodules",
-                                "OpenFUSIONToolkit",
-                                "examples",
-                                "TokaMaker",
-                                "DIIID",
-                                "g192185.02440",
-                            )
-                        ),
-                        reason="Test requires DIII-D data which is not open source",
-                    ),
-                ),
+                "eqdsk_d3d",
                 pytest.param(
                     "eqdsk_tcv",
                     marks=pytest.mark.skipif(
@@ -424,24 +392,7 @@ class TestCocos:
             "eqdsk",
             [
                 "eqdsk_cmod",
-                pytest.param(
-                    "eqdsk_d3d",
-                    marks=pytest.mark.skipif(
-                        condition=not os.path.exists(
-                            os.path.join(
-                                PACKAGE_ROOT,
-                                "..",
-                                "submodules",
-                                "OpenFUSIONToolkit",
-                                "examples",
-                                "TokaMaker",
-                                "DIIID",
-                                "g192185.02440",
-                            )
-                        ),
-                        reason="Test requires DIII-D data which is not open source",
-                    ),
-                ),
+                "eqdsk_d3d",
                 pytest.param(
                     "eqdsk_tcv",
                     marks=pytest.mark.skipif(
@@ -498,24 +449,7 @@ class TestCocos:
             "eqdsk",
             [
                 "eqdsk_cmod",
-                pytest.param(
-                    "eqdsk_d3d",
-                    marks=pytest.mark.skipif(
-                        condition=not os.path.exists(
-                            os.path.join(
-                                PACKAGE_ROOT,
-                                "..",
-                                "submodules",
-                                "OpenFUSIONToolkit",
-                                "examples",
-                                "TokaMaker",
-                                "DIIID",
-                                "g192185.02440",
-                            )
-                        ),
-                        reason="Test requires DIII-D data which is not open source",
-                    ),
-                ),
+                "eqdsk_d3d",
                 pytest.param(
                     "eqdsk_tcv",
                     marks=pytest.mark.skipif(
